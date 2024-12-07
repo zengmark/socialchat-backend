631e0b293a9d75368c53445abeeb57609062fd69
zengmark <1312320323@qq.com>
2024-12-07 23:19:03 +0800

    搭建用户模块，完成项目第一次跑通

---
+++ socialchat-backend-user-service/src/main/java/com/socialchat/UserServiceApplication.java
@@ -0,0 +1,17 @@
+package com.socialchat;
+
+import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
+@EnableDiscoveryClient
+@EnableDubbo
+public class UserServiceApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
---
+++ socialchat-backend-user-service/src/main/java/com/socialchat/controller/UserController.java
@@ -0,0 +1,13 @@
+package com.socialchat.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
+}
